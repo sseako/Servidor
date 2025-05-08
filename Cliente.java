b10120378ae785b42d34d9024f139bc07136e4c5
Diego Alfaro Alvarado <alfarojosue2405@gmail.com>
2025-05-07 19:59:25 -0600

    cambiaso7

--- Cliente.java
+++ Cliente.java
@@ -2,23 +2,12 @@ import java.io.*;
 import java.net.*;  
 public class Cliente {
 
-import java.io.*;  
-import java.net.*;  
-
- 
-    public static void main(String argv[]) throws Exception  
-    {  
-        String sentence;  
-        String modifiedSentence;  
-} 
-
     public static void main(String[] args) {
         String sentence;          
         String modifiedSentence;  
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         Socket clientSocket = new Socket("hostname", 6789);  
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
-<<<<<<< HEAD
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         sentence = inFromUser.readLine();          
         outToServer.writeBytes(sentence + '\n');          
@@ -26,9 +15,5 @@ import java.net.*;
         System.out.println("FROM SERVER: " + modifiedSentence);           
         clientSocket.close(); 
     }
-=======
 
     } // hola
-
->>>>>>> de8eac237b483bc9230910b21977540405d145ad
-}

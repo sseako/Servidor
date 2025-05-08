d17f9165d315cbc1e1a3cd47bd2f42eaec662c0b
Daniel Steven Gatgens Malespin <dgatgens7@gmail.com>
2025-05-07 19:44:13 -0600

    cam

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,11 @@
 public class Cliente {
-    
+import java.io.*;  
+import java.net.*;  
+
+ 
+    public static void main(String argv[]) throws Exception  
+    {  
+        String sentence;  
+        String modifiedSentence;  
+} 
 }

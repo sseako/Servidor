4a969dc673c8650867eabf3da7f9a6302bf8ec85
Jean Carlo Espinoza Jiménez <jeankejimenez@gmail.com>
2025-05-07 19:28:19 -0600

    guardado jk

--- Servidor.java
+++ Servidor.java
@@ -2,3 +2,15 @@
 
 
 //buenas
+
+//import java.oi.*;
+import java.net.*;
+
+class ServidorColonesDolares{
+    public static void main(String[] args) {
+
+        double clientSentence;
+        double capitalizedSentence;
+
+    }
+}

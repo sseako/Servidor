6fd8d025c7de3e19124f565b958455521bc9580d
Diego Alfaro Alvarado <alfarojosue2405@gmail.com>
2025-05-07 19:41:08 -0600

    cambio1

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,10 @@
+import java.io.*; 
+import java.net.*;  
 public class Cliente {
-    
+    public static void main(String[] args) {
+
+        String sentence;          
+        String modifiedSentence; 
+
+    }
 }

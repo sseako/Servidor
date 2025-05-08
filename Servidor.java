a239f26cf26ce4ccca42637c198e18a058b8e982
Mia Wilson <miawilson.cr@gmail.com>
2025-05-07 19:35:08 -0600

    cambio

--- Servidor.java
+++ Servidor.java
@@ -4,11 +4,8 @@
 //buenas
 //Abril
 
-<<<<<<< HEAD
 //Esto te deberia de aparecer Jorge 
 
 // Probando 123
-=======
 //Esto te deberia de aparecer Jorge
 //QUien es el mejor jugador de forniqui de libe ? 
->>>>>>> 1184afd4ed570c7f4c6356f6ac5f114acfb0bf04

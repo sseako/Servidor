de27344417ffe1c0f54ce5abf523f672935b4f8e
Jean Carlo Espinoza Jiménez <jeankejimenez@gmail.com>
2025-05-07 19:33:15 -0600

    guardado

--- Servidor.java
+++ Servidor.java
@@ -2,7 +2,7 @@
 
 
 //buenas
-<<<<<<< HEAD
+
 
 //import java.oi.*;
 import java.net.*;
@@ -15,16 +15,3 @@ class ServidorColonesDolares{
 
     }
 }
-=======
-//Abril
-
-<<<<<<< HEAD
-//Esto te deberia de aparecer Jorge 
-
-// Probando 123
-=======
-//Esto te deberia de aparecer Jorge
-//QUien es el mejor jugador de forniqui de libe ? 
->>>>>>> 1184afd4ed570c7f4c6356f6ac5f114acfb0bf04
->>>>>>> 591c6496d094c093c073b7ec7ac30e79f34814cc
-//Jorge el mejor jugador del capu... después de Dieguito

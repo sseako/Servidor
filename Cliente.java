2de0e2f19db0d10c56bf4977514f0db4a48a1f15
Diego Alfaro Alvarado <alfarojosue2405@gmail.com>
2025-05-07 20:57:43 -0600

    hola

--- Cliente.java
+++ Cliente.java
@@ -1,20 +1,29 @@
-import java.io.*;
-import java.net.*;
-
+import java.io.*; 
+import java.net.*;  
 public class Cliente {
 
-    public static void main(String[] args) throws IOException {
-        String sentence;
-        String modifiedSentence;
+    public static void main(String[] args) throws IOException{
+        String sentence;          
+        String modifiedSentence;  
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("10.153.159.246", 6789);
-        System.out.println("Ingrese un valor");
+        Socket clientSocket = new Socket("10.153.159.246", 6789); 
+    //System.out.println("Ingrese un valor"); 
         sentence = inFromUser.readLine();
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        outToServer.writeBytes(sentence + '\n');
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
-        clientSocket.close();
-    }
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));       
+        outToServer.writeBytes("1000" + '\n');          
+        modifiedSentence = inFromServer.readLine(); 
+        System.out.println("FROM SERVER: " + modifiedSentence);           
+        clientSocket.close();    
+} 
 }
+       
+    
+
+
+
+
+
+
+
+

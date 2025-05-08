1c7bed02cfa4b48daa405c45cd748d79b4cb8fe9
dsanchezGIT <sanchezad@gmail.com>
2025-05-08 06:26:08 -0600

    Corrección del profe

--- Cliente.java
+++ Cliente.java
@@ -1,21 +1,31 @@
 import java.io.*; 
 import java.net.*;  
 public class Cliente {
-
-    public static void main(String[] args) throws IOException{
-        String sentence;          
+    public static void main(String[] args) throws IOException {
+        String sentence="";          
         String modifiedSentence;  
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("10.153.159.246", 6789); 
-    //System.out.println("Ingrese un valor"); 
-        sentence = inFromUser.readLine();
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  
-        System.out.println("Enviando valor al servidor");    
-        outToServer.writeBytes("1000" + '\n');
-        System.out.println("Valor enviado al servidor");         
-        modifiedSentence = inFromServer.readLine(); 
-        System.out.println("FROM SERVER: " + modifiedSentence);           
+
+        //Uso del socket remotamente. La ip va a cambiar de acuerdo al lugar donde esté conectado.
+        //Socket clientSocket = new Socket("10.153.159.246", 6789); 
+
+        // Uso del socket localmente.
+        Socket clientSocket = new Socket("localhost", 6789);
+        try 
+        {
+            System.out.println("Ingrese un valor"); 
+            sentence = inFromUser.readLine();
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  
+            System.out.println("Enviando valor al servidor");    
+            outToServer.writeBytes(sentence + '\n');
+            System.out.println("Valor enviado al servidor");         
+            modifiedSentence = inFromServer.readLine(); 
+            System.out.println("FROM SERVER: " + modifiedSentence);         
+        }
+        catch (Exception e) {
+            e.getMessage();
+        }  
         clientSocket.close();    
 } 
 }

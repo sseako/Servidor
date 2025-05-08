fe812cf2700037e07f525338c5129ed78776f41c
Diego Alfaro Alvarado <alfarojosue2405@gmail.com>
2025-05-07 20:34:10 -0600

    cambio2

--- Cliente.java
+++ Cliente.java
@@ -2,16 +2,16 @@ import java.io.*;
 import java.net.*;  
 public class Cliente {
 
-    public static void main(String[] args) {
-        double sentence;          
-        double modifiedSentence;  
+    public static void main(String[] args) throws IOException{
+        String sentence;          
+        String modifiedSentence;  
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket(10.153.159.246, 6789);  
+        Socket clientSocket = new Socket("10.153.159.246", 6789);  
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        sentence = Double.parseDouble(inFromUser.readLine());          
+        sentence = inFromUser.readLine();          
         outToServer.writeBytes(sentence + '\n');          
-        modifiedSentence = Double.parseDouble(inFromServer.readLine()); 
+        modifiedSentence = inFromServer.readLine(); 
         System.out.println("FROM SERVER: " + modifiedSentence);           
         clientSocket.close();    
 } 

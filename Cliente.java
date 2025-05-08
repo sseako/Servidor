f948b4101aff5a0cc665c7ec73eb4c015c9db891
Daniel Steven Gatgens Malespin <dgatgens7@gmail.com>
2025-05-07 20:24:20 -0600

    mejorar

--- Cliente.java
+++ Cliente.java
@@ -1,18 +1,6 @@
 import java.io.*; 
 import java.net.*;  
 public class Cliente {
-
-<<<<<<< HEAD
- 
-    public static void main(String argv[]) throws Exception  
-    {  
-        double cantidad;          
-          
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("hostname", 6789);  
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
-
-=======
     public static void main(String[] args) {
         double sentence;          
         double modifiedSentence;  

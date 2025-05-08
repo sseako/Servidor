9e76427001be2da048a1ed78ed2e706dfba143d1
Diego Alfaro Alvarado <alfarojosue2405@gmail.com>
2025-05-07 20:48:00 -0600

    si

--- Cliente.java
+++ Cliente.java
@@ -6,7 +6,9 @@ public class Cliente {
         String sentence;          
         String modifiedSentence;  
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("10.153.159.246", 6789);  
+        Socket clientSocket = new Socket("10.153.159.246", 6789); 
+        System.out.println("Ingrese un valor"); 
+        sentence = inFromUser.readLine();
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         sentence = inFromUser.readLine();          
